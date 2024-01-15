ca84c7fa595481d824d22dcae5cea7644e79b045
Veirt <veirt@duck.com>
2024-01-16 02:21:46 +0800

    feat(client): add display weekend toggle

--- client/src/lib/components/WeekDayView.svelte
+++ client/src/lib/components/WeekDayView.svelte
@@ -1,24 +1,40 @@
 <script lang="ts">
     export let currentDay: number
 
-    const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    const days = [
+        { name: "Monday", index: 1 },
+        { name: "Tuesday", index: 2 },
+        { name: "Wednesday", index: 3 },
+        { name: "Thursday", index: 4 },
+        { name: "Friday", index: 5 },
+        { name: "Saturday", index: 6 },
+        { name: "Sunday", index: 0 },
+    ]
+
+    let displayWeekend = false
 </script>
 
 <div class="flex flex-row mt-5">
-    {#each dayNames as day, idx (idx)}
+    {#each days as day (day.index)}
         <button
-            data-day-idx={idx}
-            on:click={() => (currentDay = idx)}
-            class:today={currentDay === idx}
-            class="p-2 border cursor-pointer b-secondary"
+            data-day-idx={day.index}
+            on:click={() => (currentDay = day.index)}
+            class:active={currentDay === day.index}
+            class:weekend={!displayWeekend && (day.index === 0 || day.index === 6)}
+            class="p-2 border cursor-pointer min-w-25 b-secondary"
         >
-            {day}
+            {day.name}
         </button>
     {/each}
 </div>
+<div class="my-3"><input bind:checked={displayWeekend} type="checkbox" /> Display Weekend?</div>
 
 <style lang="postcss">
-    .today {
+    .active {
         @apply bg-secondary;
+    }
+
+    .weekend {
+        @apply hidden;
     }
 </style>
