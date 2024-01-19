e471737fb16e1a0b07250b4af226e44e89f96cb8
Veirt <veirt@duck.com>
2024-01-19 19:00:00 +0800

    fix: use `PUBLIC_SERVER_URL` env variable if available

--- client/src/lib/utils/host.ts
+++ client/src/lib/utils/host.ts
@@ -1,7 +1,8 @@
 import { dev } from "$app/environment"
+import { PUBLIC_SERVER_URL } from "$env/static/public"
 
 export function getHost(window: Window) {
-    let host = window.location.host
+    let host = PUBLIC_SERVER_URL || window.location.host
     if (dev) {
         host = `http://${window.location.hostname}:3000`
     }
