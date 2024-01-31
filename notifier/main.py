7e6f66b2eb51b072baa6ef4b37706a4a5925b280
Veirt <veirt@duck.com>
2024-01-31 22:34:38 +0800

    refactor(notifier): rewrite with class

--- notifier/main.py
+++ notifier/main.py
@@ -6,12 +6,14 @@ import requests
 import logging
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
+from utils import schedule_every
 
 load_dotenv()
 
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s] [%(levelname)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
 )
 
 if os.environ.get("SCHEDULE_API_URL") is None:
@@ -32,82 +34,64 @@ def notify(course):
     )
 
 
-def schedule_every(day_index):
-    match day_index:
-        case 0:
-            return schedule.every().sunday
-        case 1:
-            return schedule.every().monday
-        case 2:
-            return schedule.every().tuesday
-        case 3:
-            return schedule.every().wednesday
-        case 4:
-            return schedule.every().thursday
-        case 5:
-            return schedule.every().friday
-        case 6:
-            return schedule.every().saturday
-
-    raise Exception("Invalid day index")
+SCHEDULE_FILE = "schedule.json"
 
 
-current_schedule = {}
+class ScheduleManager:
+    def __init__(self):
+        logging.info("Initializing schedule")
+        self.current_schedule = {}
+
+    # Resets the schedule. Called when initialising the program and when the schedule has changed.
+    def reset_schedule(self):
+        schedule.clear("college_schedule")
+        for day_index, sch in self.current_schedule.items():
+            for entry in sch:
+                if "type" in entry and (
+                    entry["type"] == "transition-before"
+                    or entry["type"] == "cancellation"
+                ):
+                    continue
+                # subtract 30 minutes from the start time
+                initial_datetime = datetime.strptime(entry["startTime"], "%H:%M")
+                new_datetime = initial_datetime - timedelta(minutes=30)
+                start_time = new_datetime.strftime("%H:%M")
+
+                schedule_every(int(day_index)).at(start_time).do(
+                    notify, f"{entry['course']}"
+                ).tag("college_schedule")
+
+    def get_schedule(self):
+        try:
+            res = requests.get(os.environ["SCHEDULE_API_URL"])
+            res.raise_for_status()
+            new_schedule_data = res.json()["data"]
+        except Exception as e:
+            logging.error(f"Cannot access Schedule API. Error: {e}")
+            return
+
+        existing_schedule_data = {}
+        if os.path.exists(SCHEDULE_FILE):
+            with open(SCHEDULE_FILE, "r") as f:
+                existing_schedule_data = json.load(f)
+
+        # handle setting schedule when first time launching
+        if self.current_schedule == {}:
+            self.current_schedule = existing_schedule_data
+            self.reset_schedule()
+
+        if existing_schedule_data != new_schedule_data:
+            logging.info("Schedule(s) have changed.")
+            with open(SCHEDULE_FILE, "w") as f:
+                json.dump(new_schedule_data, f)
+            self.current_schedule = new_schedule_data
+            self.reset_schedule()
 
 
-# Resets the schedule. Called when initialising the program and when the schedule has changed.
-def reset_schedule():
-    schedule.clear("college_schedule")
-    for day_index, sch in current_schedule.items():
-        for entry in sch:
-            if "type" in entry and (
-                entry["type"] == "transition-before" or entry["type"] == "cancellation"
-            ):
-                continue
-            # subtract 30 minutes from the start time
-            initial_datetime = datetime.strptime(entry["startTime"], "%H:%M")
-            new_datetime = initial_datetime - timedelta(minutes=30)
-            start_time = new_datetime.strftime("%H:%M")
-
-            schedule_every(int(day_index)).at(start_time).do(
-                notify, f"{entry['course']}"
-            ).tag("college_schedule")
-
-
-def get_schedule():
-    global current_schedule
-    # logging.info("Getting schedule...")
-
-    try:
-        res = requests.get(os.environ["SCHEDULE_API_URL"])
-    except Exception as e:
-        logging.error(f"Cannot access Schedule API. Error: {e}")
-        return
-
-    if os.path.exists("schedule.json"):
-        with open("schedule.json", "r") as f:
-            if current_schedule == {}:
-                current_schedule = json.load(f)
-                reset_schedule()
-    else:
-        with open("schedule.json", "w") as f:
-            json.dump(res.json()["data"], f)
-            current_schedule = res.json()["data"]
-
-        return
-
-    if current_schedule != res.json()["data"]:
-        logging.info("Schedule(s) have changed.")
-
-        with open("schedule.json", "w") as f:
-            json.dump(res.json()["data"], f)
-            current_schedule = res.json()["data"]
-
-        reset_schedule()
-
-
-schedule.every(5).seconds.do(get_schedule).tag("get_schedule")
+schedule_manager = ScheduleManager()
+schedule.every(5).seconds.do(schedule_manager.get_schedule).tag("get_schedule")
 
 while True:
+    logging.debug(schedule.get_jobs())
     schedule.run_pending()
     time.sleep(1)
