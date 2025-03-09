a0da19847f8cdc2118039ad0269986534c815f6d
Veirt <veirt@duck.com>
2025-03-09 23:32:03 +0800

    feat(server): serve frontend

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,11 @@
 import { Hono } from "hono"
+import { serveStatic } from "hono/bun"
 import { cors } from "hono/cors"
 import { checkRequiredVariables } from "./config/env"
-import oauthRouter from "./routes/oauth.route"
-import scheduleRouter from "./routes/schedule.route"
 import "./notifier/index"
 import authRouter from "./routes/auth.route"
+import oauthRouter from "./routes/oauth.route"
+import scheduleRouter from "./routes/schedule.route"
 
 checkRequiredVariables()
 
@@ -21,5 +22,7 @@ app.use(
 app.route("/api/schedule", scheduleRouter)
 app.route("/api/auth", authRouter)
 app.route("/api/oauth", oauthRouter)
+
+app.use("*", serveStatic({ root: "./frontend" }))
 
 export default app
